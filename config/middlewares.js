4c05307248739b71246f222e9776f925fef1911c
Abdelrahman Sakr <abdelrahmansakr199@gmail.com>
2025-06-14 23:54:55 +0300

    first-commit

---
+++ config/middlewares.js
@@ -0,0 +1,27 @@
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": [
+            "'self'",
+            "https://api.cloudinary.com",
+            "http://localhost:3000",
+          ],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com"],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
